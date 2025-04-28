6811243efb3d22980f088a287a3a53a1e6c3b18f
mhsnabz <44256962+mhsnabz@users.noreply.github.com>
2025-04-28 20:37:50 +0300

    created project

---
+++ ProductList-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductList-ios
+//
+//  Created by srbrt on 28.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
